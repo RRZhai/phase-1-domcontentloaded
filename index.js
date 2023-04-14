e9c735b45410f904af0d4433df9fd16979346ac9
ShiyaoZhai <zsshiyaozhai@gmail.com>
2023-04-13 21:25:23 -0700

    change inner text

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("This is really cool!");
+    const coolP = document.querySelector("#text");
+    coolP.textContent = "This is really cool!"
+})
